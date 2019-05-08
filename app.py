7fd17a81c2a4401719576b24235f8eacda7f6117
charliedtx <corbintx@gmail.com>
2019-05-07 22:32:06 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,67 @@
+#imports
 from flask import Flask
+from uuid import uuid4
+from flask import abort, request, render_template
+import requests
+import requests.auth
 
-app = Flask(__name__)
+#client variables
+CLIENT_ID = "52QRYDErEdzcipF5eAqnqgJYZZ1xJHtM"
+CLIENT_SECRET = "qxxnWnXE1q00sk9c"
+REDIRECT_URI = "https://ourappname.herokuapp.com/login/index.html"
+
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def hello():
-    return "Sup, guys."
+def home():
+    text = '<a href="%s">enter authorization credentials</a>'
+    return text % make_authorization_url
+
+def make_authorization_url
+    state = str(uuid4())
+    save_created_state(state)
+    params = {"client_id": CLIENT_ID,
+              "response_type": "code",
+              "state": state,
+              "redirect_uri": REDIRECT_URI,
+              "duration": "temporary",
+              "score": "identity"}
+    import urllib
+    url = "https://api.dexcom.com/v2/oauth2/login?" + urllib.urlencode(params)
+    return url
+
+@app.route('/login')
+def login():
+    error = request.args.get('error', '')
+    if error:
+        return "We ran into an issue with your login: " + error
+
+    state = request.args.get('state', '')
+    if not is_valit_state(state):
+        abort(403)
+    code = request.args.get('code')
+
+    return "got an access token! %s" % get_token(code)
+
+    #return render_template "index.html"
+
+def get_token(code):
+    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+    post_data = {"grant_type": "authorization_code",
+                 "code": code,
+                 "redirect_uri": REDIRECT_URI}
+    response = requests.post("https://api.dexcom.com/v2/oauth2/token",
+                             auth=client_auth,
+                             data=post_data)
+
+    token_json = response.json()
+    return token_json["access_token"]
+
+def data_pull(access_token):
+    headers = {"Authorization": "bearer" + access_token}
+    #response = requests.get
 
 if __name__ == '__main__':
-    app.run()
+
+    port = 8000 + random.randint(0, 999)
+    app.run(port=port, debug=False)
