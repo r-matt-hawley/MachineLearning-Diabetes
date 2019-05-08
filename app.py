9d060aa78d4ee89d3c0d71849aa4347176d09121
charliedtx <28380954+charliedtx@users.noreply.github.com>
2019-05-08 18:47:19 -0500

    update to app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def make_authorization_url():
                 "duration": "temporary",
                 "score": "identity"}
 
+    import urllib
     url = "https://api.dexcom.com/v2/oauth2/login?" + urllib.urlencode(params)
     return url
 
