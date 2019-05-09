cbb05082cf7975718594a059db4c99d7b17f70d6
charliedtx <28380954+charliedtx@users.noreply.github.com>
2019-05-08 19:18:46 -0500

    update to app.py

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ def home():
 
 def make_authorization_url():
     state = str(uuid4())
-    #save_created_state(state)
+    save_created_state(state)
     params = {"client_id": CLIENT_ID,
                 "response_type": "code",
                 "state": state,
                 "redirect_uri": REDIRECT_URI,
                 "duration": "temporary",
-                "score": "identity"}
+                "scope": "identity"}
 
     url = "https://api.dexcom.com/v2/oauth2/login?" + urlencode(params)
     return url
