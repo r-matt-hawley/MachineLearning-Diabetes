4fed0b146c4916fbc906e907db4960cb5bf49886
charliedtx <28380954+charliedtx@users.noreply.github.com>
2019-05-15 19:46:25 -0500

    update to app.py

--- app.py
+++ app.py
@@ -43,14 +43,16 @@ def login():
     code = request.args.get('code')
     access_token = get_token(code)
 
-    return "got a token! %s" % get_token(code)
+    return "got a token! %s" % get_readings(access_token)
 
 def get_token(code):
     payload = {'client_secret': CLIENT_SECRET,
                'client_id': CLIENT_ID,
                'code':code,
                'grant_type': "authorization_code",
-               'redirect_uri': REDIRECT_URI}
+               'redirect_uri': REDIRECT_URI,
+               'content-type': "application/x-www-form-urlencoded",
+               'cache-control': "no-cache"}
 #    headers = {
 #            'content-type': "application/x-www-form-urlencoded",
 #            'cache-control': "no-cache"}
@@ -60,11 +62,11 @@ def get_token(code):
     token_json = response.json()
     return token_json
 
-#def get_readings(access_token):
-#    headers = {'authorization': "Bearer" + access_token}
-#    response = requests.get("https://api.dexcom.com/v2/users/self/egvs?startDate=?startDate=2017-01-01T00:00:00&endDate=2019-05-01T00:00:00", headers=headers)
-#    data_json = response.json()
-#    return data_json
+def get_readings(access_token):
+    headers = {'authorization': "Bearer" + access_token}
+    response = requests.get("https://api.dexcom.com/v2/users/self/egvs?startDate=?startDate=2017-01-01T00:00:00&endDate=2019-05-01T00:00:00", headers=headers)
+    data_json = response.json()
+    return data_json
 
 if __name__ == '__main__':
     app.run(debug=True, port=65010)
