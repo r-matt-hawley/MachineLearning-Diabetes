41ed1958d5b88b5b681f52f86d9e92cdcfdfcb38
charliedtx <28380954+charliedtx@users.noreply.github.com>
2019-05-15 20:47:44 -0500

    update to app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def get_readings(access_token):
 
 #    headers = {'authorization': "Bearer " + access_token}
 
-    conn.request("GET", "/v2/users/self/egvs?startDate=2017-06-16T15:30:00&endDate=2017-06-16T15:45:00", headers={'authorization': "Bearer " + access_token})
+    conn.request("GET", "/v2/users/self/egvs?startDate=2017-06-16T15:30:00&endDate=2017-06-16T15:45:00", headers=f"{'authorization': "Bearer " + {access_token}")
 
     res = conn.getresponse()
     data = res.read()
