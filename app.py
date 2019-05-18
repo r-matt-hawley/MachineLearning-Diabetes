27f1b084a0c3a433ab1b96a7bebc82e7765df3e3
charliedtx <28380954+charliedtx@users.noreply.github.com>
2019-05-18 09:35:02 -0500

    update to app

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def get_data(access_token):
     print(sep)
     #######################################
 
-    return me_json['unit'] + json.dumps(me_json["egvs"])
+    return me_json['unit']
 
 @app.route('/plots')
 def plots():
